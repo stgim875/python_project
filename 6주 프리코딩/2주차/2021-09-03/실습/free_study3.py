7fbadda44dfe4d3e16dee1f7a2afc09015807392
stgim <stgim@mobigen.com>
2021-09-10 13:25:18 +0900

    2주차 교과목 학점 평가 시스템 코드 구현

--- 6주 프리코딩/2주차/2021-09-03/실습/free_study3.py
+++ 6주 프리코딩/2주차/2021-09-03/실습/free_study3.py
@@ -2,33 +2,28 @@
 
 print("교과목 학점 평가 시스템입니다.")
 
-test_score = input("시험 점수를 입력해 주세요")
-study_score = input("과제 점수를 입력해 주세요")
+test_score = input("시험 점수를 입력해 주세요\n")
+study_score = input("과제 점수를 입력해 주세요\n")
 
 # # 시험점수와 과제점수 정수 입력
 test_score = int(test_score)
 study_score = int(study_score)
+score = int(score)
 
-if test_score >= 70:
-    test_score = 0
-elif test_score >= 50:
-    test_score = 0
-elif test_score >= 30:
-    test_score = 0
-elif test_score < 30:
-    test_score = 0
+# 총 점수 계산 방법
+score = test_score + study_score
+
+# 조건문
+if score >= 70:
+    score = "A"
+elif score >= 50:
+    score = "B"
+elif score >= 30:
+    score = "C"
+elif score <= 30:
+    score = "D"
+elif score <= 10:
+    score = "F"
 else:
-    test_score = "잘못된 입력"
-
-if study_score >= 20:
-    study_score = 0
-elif study_score >= 15:
-    study_score = 0
-elif study_score >= 10:
-    study_score = 0
-elif study_score < 10:
-    study_score = 0
-else:
-    study_score = "잘못된 입력"
-
-print("당신의 점수는 {0}점 입니다.".format(test_score+study_score))
+    score = "잘못된 입력입니다."
+print("당신의 점수는 {0}점 .".format(score))
