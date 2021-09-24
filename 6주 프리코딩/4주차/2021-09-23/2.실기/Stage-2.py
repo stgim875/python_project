096fc4944e3ee4dec093c75fd35ff9503f14d698
stgim <stgim@mobigen.com>
2021-09-24 11:57:45 +0900

    4주차 실습 업데이트

--- 6주 프리코딩/4주차/2021-09-23/2.실기/Stage-2.py
+++ 6주 프리코딩/4주차/2021-09-23/2.실기/Stage-2.py
@@ -11,7 +11,8 @@ print(new_number)
 # 랜덤하게 4자리의 숫자 만들기(각 자리의 수는 중복 불가)
 answer = list()
 
-# 번호 중복 제거 및 검사하기
+# 번호 중복을 피하고 중복 검사하기
+# 리스트에 값이 추가 되거나, 삭제 될 경우, 'len(answer)'라고 적혀 있는 값을 리스트에 있는 값이 몇개냐에 따라서 달라지도록 가변적으로 바꿔주는 명령어가 바로 'len'임
 while len(answer) != 4:
     new_number = random.randint(0, 9)
     if new_number not in answer:
